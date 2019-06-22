74aace5fb541688db0805403aa25df204d50ebb7
Abhishek Bansal <doscover.ab@gmail.com>
2019-06-22 10:27:46 +0530

    + more code for Kotlin Demo

--- app/src/main/java/com/abhishek/myapplication/MainActivity.kt
+++ app/src/main/java/com/abhishek/myapplication/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.abhishek.myapplication
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.properties.Delegates.observable
 
 class MainActivity : AppCompatActivity() {
+
+    val animationDuration: Int by lazy {
+        100 * 10
+    }
+
+    var statusText: String by observable("") { prop, old, new ->
+        statusTv?.text = new
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        container.postDelayed ( {
-            container.slideInFromBottom(animateAlpha = true)
+//        statusText = "Waiting for animation to start"
+        container.postDelayed({
+//            statusText = "Running Animation"
+            container.slideInFromBottom()
         }, 1000)
     }
 }
--- app/src/main/java/com/abhishek/myapplication/ViewExtensions.kt
+++ app/src/main/java/com/abhishek/myapplication/ViewExtensions.kt
@@ -1,7 +1,9 @@
 package com.abhishek.myapplication
 
+import android.content.Context
 import android.view.View
 import android.view.animation.DecelerateInterpolator
+import android.widget.Toast
 
 fun View.setVisible(isVisible: Boolean) {
     visibility = if (isVisible) View.VISIBLE else View.GONE
@@ -29,4 +31,8 @@ fun View.slideInFromBottom(animationDuration: Long = 300, animateAlpha: Boolean 
         .withEndAction(endAction)
 
     setVisible(true)
+}
+
+fun String.showToast(context: Context, length: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(context, this, length).show()
 }
