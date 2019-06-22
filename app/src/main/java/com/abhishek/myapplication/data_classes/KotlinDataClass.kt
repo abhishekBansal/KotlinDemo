cc5775512486788c126186e0cad316e384857a10
Abhishek Bansal <doscover.ab@gmail.com>
2019-06-22 13:07:02 +0530

    + experimenta true

--- app/src/main/java/com/abhishek/myapplication/data_classes/KotlinDataClass.kt
+++ app/src/main/java/com/abhishek/myapplication/data_classes/KotlinDataClass.kt
@@ -1,5 +1,8 @@
 package com.abhishek.myapplication.data_classes
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class KotlinDataClass(val name: String, val age: Int, val address: String, val fatherName: String)
 //class abc
 //class pqr: abc()
