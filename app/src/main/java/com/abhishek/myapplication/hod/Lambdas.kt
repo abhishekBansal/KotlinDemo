ea2c65cf0c5dbd0afed93a1e4d7e25aaa3ec929e
viveksingh <vs.thaakur@gmail.com>
2019-06-22 11:13:10 +0530

    Lambda and HOF comparison with Java

--- app/src/main/java/com/abhishek/myapplication/hod/Lambdas.kt
+++ app/src/main/java/com/abhishek/myapplication/hod/Lambdas.kt
@@ -2,18 +2,26 @@ package com.abhishek.myapplication.hod
 
 import android.util.Log
 
+
+
 class Lambdas {
-    val lambda = { print("Empty Lambda take no argument and return nothing") }
+
+    val lambda = { println("No argument No return lambda") }
     val lambda1 = { arg1: Int -> print("Empty Lambda take $arg1 Integer argument and return nothing") }
-    val lambda1Return = { arg1: Int ->
-        print("Empty Lambda take $arg1 Integer argument and return its square ")
-        arg1 * arg1
-    }
+    val lambda1Return = { arg1: Int -> arg1 * arg1 }
 
     fun main() {
-        val value0 = lambda()
-        val value1 = lambda1(2)
-        val value2 = lambda1Return(2)
+        val value0 = lambda
+        value0.invoke()
+
+        val value1 = lambda1
+        lambda1(2)
+
+        val value2 = lambda1Return
+        val answer = value2(9)
+
+        val a = this::lamdaReturn
+        a()
     }
 
 
---
+++ app/src/main/java/com/abhishek/myapplication/hod/LambdasInJava.java
@@ -0,0 +1,31 @@
+package com.abhishek.myapplication.hod;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class LambdasInJava {
+
+    Runnable lambda = () -> System.out.println("No argument No return lambda");
+    Consumer<Integer> lambda1 = (arg1) -> System.out.println("One argument No return lambda");
+    Function<Integer, Integer> lambda1Return = (arg1) -> arg1 * arg1;
+
+    void main() {
+        Runnable value0 = lambda;
+        lambda.run();
+
+        Consumer<Integer> value1 = lambda1;
+        value1.accept(2);
+
+        Function<Integer, Integer> value2 = lambda1Return;
+        value2.apply(2);
+
+        Supplier<String> a = lambdaReturn();
+        a.get();
+    }
+
+    Supplier<String> lambdaReturn() {
+        String abc = "abc";
+        return () -> abc;
+    }
+}
